1f685f29191d4f41f3a70f49de9c515a200152d4
Alvaa11 <alvaro02.rodrigues@gmail.com>
2025-04-14 17:44:49 -0300

    Debuging

--- controller/bot_desactivate.py
+++ controller/bot_desactivate.py
@@ -1,7 +1,6 @@
 import controller.bot_func as bf
 from time import sleep
 from rubs import rubs_list
-import controller.bot_logoff as bl
 
 
 
@@ -30,6 +29,21 @@ class BotRubs():
 
         bf.soltarTecla('ctrl')
 
-        # Chama o proximo bot responsável pelo logoff do usuario
-        bot = bl.BotLogOff()
-        bot.log_off()
+        # Faz a desvinculação dos rubs
+        bf.moverMouse(x=411, y=42)
+        bf.clicar()
+        sleep(0.5)
+        bf.moverMouse(x=851, y=347)
+        bf.clicar()
+        sleep(1)
+
+        # Sai do login do usuário e fecha o Browser
+        bf.moverMouse(x=1509, y=9)
+        bf.clicar()
+        sleep(0.5)
+        bf.moverMouse(x=1510, y=124)
+        bf.clicar()
+        sleep(0.5)
+        bf.pressionarTecla('f11')
+        bf.pressionarTeclas('alt', 'f4')
+
